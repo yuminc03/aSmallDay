97408412a4b2821dbcb1eaefb5d57293f75dbd7d
yuminc03 <yuminc03@gmail.com>
2021-05-30 20:53:55 +0900

    2021-5-30: 회원가입 기능 404에러 수정하기!

--- login_backend/src/index.js
+++ login_backend/src/index.js
@@ -1,5 +1,5 @@
-//이 파일에서만 no-global-assign ESLinet 옵션을 비활성화한다
-/*eslint-disable no-global-assign */
+// 이 파일에서만 no-global-assign ESLint 옵션을 비활성화합니다
+/* eslint-disable no-global-assign */
 
 require = require('esm')(module /*, options*/);
 module.exports = require('./main.js');

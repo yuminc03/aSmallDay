b8d3c8046e2a99458bcf7f7f0957851499ae649c
yuminc03 <yuminc03@gmail.com>
2021-05-31 01:09:33 +0900

    2021-05-31: 포스트 목록 조회 API 연동하기

---
+++ login_frontend/src/containers/posts/PostListContainer.js
@@ -0,0 +1,38 @@
+//주소에 있는 쿼리 파아미터를 추출하여 listPosts API를 호출한다
+import React, { useEffect } from 'react';
+import qs from 'qs';
+import { withRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import PostList from '../../components/posts/PostList';
+import { listPosts } from '../../modules/posts';
+
+const PostListContainer = ({ location, match }) => {
+  const dispatch = useDispatch();
+  const { posts, error, loading, user } = useSelector(
+    ({ posts, loading, user }) => ({
+      posts: posts.posts,
+      error: posts.error,
+      loading: loading['posts/LIST_POSTS'],
+      user: user.user,
+    }),
+  );
+  useEffect(() => {
+    const { username } = match.params;
+    const { tag, page } = qs.parse(location.search, {
+      ignoreQueryPrefix: true,
+    });
+    dispatch(listPosts({ tag, username, page }));
+  }, [dispatch, location.search]);
+
+  return (
+    <PostList
+      loading={loading}
+      error={error}
+      posts={posts}
+      showWriteButton={user}//props를 현재 로그인 중인 사용자의 정보를 지니고 있는 user객체로 설정
+      //user객체가 유효할 때(사용자가 로그인 중일 때만) 포스트를 작성하는 버튼이 나타남
+    />
+  );
+};
+
+export default withRouter(PostListContainer);
